3280eca13fe53b5ed580273a52b4b411785d6443
SamHL <samuel_hislop-lynch@hotmail.com>
2016-11-24 12:44:49 +1000

    dev

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,145 @@
-﻿// Include directories
+﻿#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+using namespace cv;
+using namespace std;
+
+/// Global variables
+Mat src, src_gray;
+
+int maxCorners = 23;
+int maxTrackbar = 450;
+
+RNG rng(12345);
+char* source_window = "Image";
+
+/// Function header
+void goodFeaturesToTrack_Demo(int, void*);
+
+/**
+* @function main
+*/
+int main(int argc, char** argv)
+{
+	// Get filepath to video
+	string fileDirectory = "C:\\Users\\uqshislo\\Dropbox\\BAC_Project_Sam\\110 - Video Data Collection\\BAC Domestic Terminal Data Collection\\20160915\\Domestic\\Virgin\\";
+	string fileName = "BAC_DOMESTIC_VIRGIN_20160915_GANTRY2_3of3.MP4";
+
+	Mat frame;
+	vector<Point> features;
+
+	// Loop through the video and show detections
+	VideoCapture cap(fileDirectory + fileName);
+	cap.set(CV_CAP_PROP_POS_FRAMES, 100);
+
+
+	while (cap.isOpened())
+	{
+		cap >> frame;
+
+		// Load source image and convert it to gray
+		src = frame;
+		cvtColor(src, src_gray, CV_BGR2GRAY);
+
+		// Create Window
+		namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+
+		// Create Trackbar to set the number of corners
+		createTrackbar("Max  corners:", source_window, &maxCorners, maxTrackbar, goodFeaturesToTrack_Demo);
+
+		imshow(source_window, src);
+
+		goodFeaturesToTrack_Demo(0, 0);
+
+		waitKey(50);
+
+	}
+
+
+	return(0);
+}
+
+/**
+* @function goodFeaturesToTrack_Demo.cpp
+* @brief Apply Shi-Tomasi corner detector
+*/
+void goodFeaturesToTrack_Demo(int, void*)
+{
+	if (maxCorners < 1) { maxCorners = 1; }
+
+	/// Parameters for Shi-Tomasi algorithm
+	vector<Point2f> corners;
+	double qualityLevel = 0.01;
+	double minDistance = 10;
+	int blockSize = 3;
+	bool useHarrisDetector = false;
+	double k = 0.04;
+
+	/// Copy the source image
+	Mat copy;
+	copy = src.clone();
+
+	/// Apply corner detection
+	goodFeaturesToTrack(src_gray,
+		corners,
+		maxCorners,
+		qualityLevel,
+		minDistance,
+		Mat(),
+		blockSize,
+		useHarrisDetector,
+		k);
+
+
+	/// Draw corners detected
+	cout << "** Number of corners detected: " << corners.size() << endl;
+	int r = 4;
+	for (int i = 0; i < corners.size(); i++)
+	{
+		circle(copy, corners[i], r, Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
+			rng.uniform(0, 255)), -1, 8, 0);
+	}
+
+	/// Show what you got
+	namedWindow(source_window, CV_WINDOW_KEEPRATIO);
+	imshow(source_window, copy);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Include directories
 // ================================================================
 #include <qapplication.h>
 #include <qobject.h>
@@ -48,7 +189,7 @@ int main(int argc, char** argv)
 	hog.setSVMDetector(supportVectors);
 	
 	// Get filepath to video
-	string fileDirectory = "C:\\Users\\Samue\\Dropbox\\BAC_Project_Sam\\110 - Video Data Collection\\BAC Domestic Terminal Data Collection\\20160915\\Domestic\\Virgin\\";
+	string fileDirectory = "C:\\Users\\uqshislo\\Dropbox\\BAC_Project_Sam\\110 - Video Data Collection\\BAC Domestic Terminal Data Collection\\20160915\\Domestic\\Virgin\\";
 	string fileName = "BAC_DOMESTIC_VIRGIN_20160915_AUTOMATEDKIOSK2_3of3.MP4";
 
 	// Create descriptor and detection variables
@@ -61,14 +202,14 @@ int main(int argc, char** argv)
 
 	// Loop through the video and show detections
 	VideoCapture cap(fileDirectory + fileName);
-	Mat frame;
+	Mat frame, greyFrameI, greyFrameO;
+	vector<Point> features;
+
 	cap.set(CV_CAP_PROP_POS_FRAMES, 100);
 
 	while (cap.isOpened())
 	{
 		cap >> frame;
-		imshow("Input", frame);
-		cv::waitKey(2);
 
 		// Detect
 		hog.detect(frame, detections);
@@ -79,10 +220,16 @@ int main(int argc, char** argv)
 			cv::Rect2i rect(detections.at(i).x, detections.at(i).y, 96, 160);
 			cv::rectangle(frame, rect, cv::Scalar(0, 255, 0), 1);
 		}
-		imshow("Features", frame);
+		imshow("Input", frame);
 		cv::waitKey(500);
 
 		// Calculate corners to be used for detection..
+		cv::cvtColor(frame, greyFrameI, CV_BGR2GRAY);
+		cv::cornerHarris(greyFrameI, greyFrameO, 2, 3, 0.04);
+
+		imshow("Features", greyFrameO);
+		cv::waitKey(500);
+
 
 		// Track corners..
 
@@ -91,3 +238,5 @@ int main(int argc, char** argv)
 
 	return 0;
 }
+
+*/
